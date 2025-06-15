ca62a8ef0ccef12ce9eae2b759ea7c37b32005b4
Dmitriy Alexandrov <newsmobileads@gmail.com>
2025-06-15 20:10:12 +0500

    Добавлены шаблоны, статика.

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = 'users/home.html'
+    extra_context = {
+        'title': 'Ваша афиша. Главная страница',
+    }
