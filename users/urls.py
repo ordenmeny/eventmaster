2b14ef68f1488458e39f8d718da013c1b081933e
Dmitriy Alexandrov <newsmobileads@gmail.com>
2025-06-16 11:28:19 +0500

    Добавлены основные страницы

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,7 @@ from .views import *
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('event/<int:pk>/', DetailEventView.as_view(), name='event_detail'),
+    path('collection/<int:pk>/', DetailCollectionView.as_view(), name='collection_detail'),
+    path('event-by-category/<int:pk>/', DetailCategoryView.as_view(), name='category_detail'),
+    path('all-colletions/', ListCollectionsView.as_view(), name='collection_list'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, DetailView
+from django.views.generic import TemplateView, DetailView, ListView
 from .models import *
 
 class HomePageView(TemplateView):
@@ -23,3 +23,29 @@ class DetailEventView(DetailView):
     template_name = 'users/event_detail.html'
     context_object_name = 'event'
 
+
+class DetailCollectionView(DetailView):
+    model = CollectionsModel
+    template_name = 'users/collection_detail.html'
+    context_object_name = 'collection'
+
+
+class DetailCategoryView(ListView):
+    model = EventModel
+    template_name = 'users/category_detail.html'
+    context_object_name = 'events'
+
+    def get_queryset(self):
+        category_id = self.kwargs['pk']
+        return EventModel.objects.filter(category_id=category_id)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = CategoryModel.objects.get(pk=self.kwargs['pk'])
+        return context
+
+
+class ListCollectionsView(ListView):
+    model = CollectionsModel
+    template_name = 'users/collection_list.html'
+    context_object_name = 'collections'
