90609695198265c0d6f8ab8103187cc8b19f3569
Dmitriy Alexandrov <newsmobileads@gmail.com>
2025-06-17 19:52:15 +0500

    delete files

--- run_uwsgi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-python manage.py collectstatic --noinput
-python manage.py migrate
-
-uwsgi --socket :9000 --workers 4 --master --enable-threads --uid root --module djangoProject.wsgi
